228e79d354f9d20dda43987b738636aa7aa6582d
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-23 12:22:04 +0100

    added limiter to amount of products

--- popular_recommendation_engine.py
+++ popular_recommendation_engine.py
@@ -8,20 +8,25 @@ con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=1
 cur = con.cursor()
 
 
+
 def pop_product_recommendation():
 	'''
-	This function generates a list containing 4 products from the table popular_products
+	This function generates a list containing 4 products from the table populaireste_prod
 	in the database. 
 	'''
 
-	cur.execute("SELECT * FROM popular_product")
+	cur.execute("SELECT * FROM populaireste_prod")
 	productlist = cur.fetchall()
 
+	productlist = productlist[:100] # limit to only the top 100 products
 	popular_products = []# will contain 4 random products from the list "pop_products"
 	
+	# shuffle list and pick the first 4 products
 	random.shuffle(productlist)
 	for product in productlist[:4]:
-		popular_products.append(product[0]) 
+		popular_products.append(product[1]) 
 	return(popular_products)
+
+
 
 
