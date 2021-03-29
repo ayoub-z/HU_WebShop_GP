425e223026af04304d092d5d0b6a1c6874d088b5
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-29 19:50:15 +0200

    Added comments/cleaned up code

--- product_combination_filler.py
+++ product_combination_filler.py
@@ -7,6 +7,8 @@ con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=1
 #cursor
 cur = con.cursor()
 
+
+
 def shopping_cart_products(product_id):
 	'''
 	Function takes a product_id as input and goes through all shopping carts that contain
@@ -31,6 +33,7 @@ def shopping_cart_products(product_id):
 
 	# sorts dictionary from top to bottom and picks only the top 4 results
 	sorted_dict = sorted(dict_shopping_cart_products.items(), key=lambda x: x[1], reverse=True)[:4]
+	# checks if there are at least 4 products bought together with the given product_id
 	if len(sorted_dict) == 4:
 		end_result = [] # will contain the top 4 products, without the dictionary values
 		end_result.append(product_id)
@@ -38,34 +41,44 @@ def shopping_cart_products(product_id):
 		# small loop to put the products in final variable
 		for product in sorted_dict:
 			end_result.append(product[0])
+
+		# adds amount of times the last/lowest product has been bought together with the given product
 		end_result.append(sorted_dict[3][1])
 		return(end_result)
 	else:
 		return('0')
+
+
 
 def product_combination_filler():
 	'''
 	Function creates and fills table "product_combination"
 	'''
 	
-	cur.execute("CREATE TABLE IF NOT EXISTS product_combination_2 (product_id VARCHAR (40) NOT NULL, combi_product_1 varchar(255) NOT NULL, combi_product_2 varchar(255) NOT NULL, \
+	# sql query that creates product_combination table
+	cur.execute("CREATE TABLE IF NOT EXISTS product_combination (product_id VARCHAR (40) NOT NULL, combi_product_1 varchar(255) NOT NULL, combi_product_2 varchar(255) NOT NULL, \
 			combi_product_3 varchar(255) NOT NULL, combi_product_4 varchar(255) NOT NULL, lowest_combi_count int4 NOT NULL, PRIMARY KEY (product_id));")
 
+	# sql query to receive all product_id's from table product
 	cur.execute("SELECT _id FROM product")
 	all_products = cur.fetchall()
 	
 	insert_count = 0
 	skip_counter = 0
 	for product in all_products:
+		# checks if there are at least 4 other products bought together with this product
 		if len(shopping_cart_products(product[0])) == 6:
-			cur.execute("INSERT INTO product_combination_2 (product_id, combi_product_1, combi_product_2, combi_product_3, combi_product_4, lowest_combi_count) VALUES (%s, %s, %s, %s, %s, %s)", shopping_cart_products(product[0]))
+			# sql insert query to fill database
+			# "lowest_combi_count" means product that has been bought together with given product_id the lowest amount of times out of top 4 results
+			cur.execute("INSERT INTO product_combination (product_id, combi_product_1, combi_product_2, combi_product_3, combi_product_4, lowest_combi_count) \
+			VALUES (%s, %s, %s, %s, %s, %s)", shopping_cart_products(product[0]))
 			insert_count += 1
 			print(f"Inserted: {insert_count} out of 11598")
 		else:
 			skip_counter += 1
 			pass
 	
-	print(f"{insert_count} inserts \n{skip_counter} products have been skipped because they haven't been bought together with enough other products" )
+	print(f"{insert_count} inserts \n{skip_counter} products have been skipped because they haven't been bought together with enough other products")
 
 product_combination_filler()
 
