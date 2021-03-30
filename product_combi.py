dec4e4211bac8f0d4eb4831395b9863b9cdafc04
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-30 13:34:50 +0200

    Small fix to product_combi.py

--- product_combi.py
+++ product_combi.py
@@ -12,9 +12,8 @@ def product_combi(lastcartproductid):
 	# sql query retrieves all 4 products that are a good combination with this product_id from table "product_combination"
 	cur.execute("SELECT * FROM product_combination WHERE product_ID = %s", (str(lastcartproductid),))
 	productlist = cur.fetchall()
-
 	# checks that the count of how often the last product is bought is at least 5 or higher
-	if productlist[0][5] > 5:
+	if productlist[0][5] >= 5:
 		prodids = productlist[0][1:5]
 		return prodids
 	else:
