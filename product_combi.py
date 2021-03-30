ce9b81e61e7b15b2d1dc17017e283d8453181d02
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-30 10:48:12 +0200

    Added doc_string/comments

--- product_combi.py
+++ product_combi.py
@@ -5,9 +5,16 @@ con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=1
 cur = con.cursor()
 
 def product_combi(lastcartproductid):
-	cur.execute("SELECT * FROM product_combination_2 WHERE product_ID = %s", (str(lastcartproductid),))
+	'''
+	Function returns the best 4 products that combine with the given product_id from shopping cart 
+	'''
+
+	# sql query retrieves all 4 products that are a good combination with this product_id from table "product_combination"
+	cur.execute("SELECT * FROM product_combination WHERE product_ID = %s", (str(lastcartproductid),))
 	productlist = cur.fetchall()
-	if productlist[0][5] >= 5:
+
+	# checks that the count of how often the last product is bought is at least 5 or higher
+	if productlist[0][5] > 5:
 		prodids = productlist[0][1:5]
 		return prodids
 	else:
