8bc0dc6bbdd22c09d8c4edee08d6d873d5ecbb92
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-30 13:34:23 +0200

    Changed login info

--- database_setup/db_connection.py
+++ database_setup/db_connection.py
@@ -1,5 +1,5 @@
 import psycopg2
 
 #connect to the db
-con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123')
-cur = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123').cursor()
+con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=12345')
+cur = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=12345').cursor()
