e7a093332251f5d1a70a735d9ff4da12c84667f9
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-03-30 10:30:51 +0200

    Added formatting function into Get_matching_prod

--- Category_Reco.py
+++ Category_Reco.py
@@ -7,6 +7,9 @@ con = psycopg2.connect('host=localhost dbname=hu_webshop user=postgres password=
 #cursor
 cur = con.cursor()
 
+sub_category = ''
+category = ''
+
 def get_matching_prod(category,sub_category):
     '''Function that receives information from Webshop
     This information contains  'Category' ,  and ' Sub-category ' when available.
@@ -16,31 +19,32 @@ def get_matching_prod(category,sub_category):
     If there is 10 or less products with matching category, go to fall_back()
     If there is no sub-category, and no category, fall back to fall_back()
     '''
-    # temp vars used while front-end connection is not fully set up yet
-    sub_category = []
-    category = []
+
     # variable list that gets filled with recommendable products
     matching_products = []
 
-    # SETUP BACK PLAN FOR WHEN LENTGTH MATCHING PROD < 10
-
     if sub_category != None:
-        cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where sub_category = '%s';",sub_category)
+        cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where sub_category = %s;",(sub_category,))
         matching_products = cur.fetchall()
         if len(matching_products) > 10:
-            cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where sub_category = '%s' LIMIT 4 ;",sub_category)
+            cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where sub_category = %s LIMIT 4 ;",(sub_category,))
             matching_products = cur.fetchall()
+            matching_products = formatting_fix(matching_products)
             return matching_products
+        else:
+            fall_back()
     if category != None:
-        cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where category = '%s';",category)
+        cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where category = %s;",(category,))
         matching_products = cur.fetchall()
         if len(matching_products) > 10:
-            cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where category = '%s' LIMIT 4;",category)
+            cur.execute("select  product._id from product inner join populairste_prod on product._id = populairste_prod.productid where category = %s LIMIT 4;",(category,))
             matching_products = cur.fetchall()
+            matching_products = formatting_fix(matching_products)
             return matching_products
+        else:
+            fall_back()
     else:
         fall_back()
-
 
 def fall_back():
     '''This function gets called from get_matchin_prod
@@ -53,3 +57,19 @@ def fall_back():
     matching_products = cur.fetchall()
     return matching_products
 
+def formatting_fix(matching_products):
+    '''this function is made to change the formatting of the product ID's given by the cur.execute in get_matching_prod
+    The original formatting after performing the execute =   [('x',),('y',),('z',),('w',)]
+    We need this to change to: [ 'x', 'y', 'z', 'w' ]
+    We need this to happen because of the way huw_reccommend uses date
+
+    '''
+
+    nieuwelijst=[]
+    for tjoeple in matching_products:
+        nieuwelijst.append(str(tjoeple[0]))
+    matching_products = nieuwelijst
+    return matching_products
+
+
+print(get_matching_prod(None,'Lichaamsverzorging'))
