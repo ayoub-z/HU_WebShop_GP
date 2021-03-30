6490a534c92338e4cdda2fd2a51e127786dbd0d1
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-30 12:11:01 +0200

    Added file db_connection.py

---
+++ database_setup/db_connection.py
@@ -0,0 +1,5 @@
+import psycopg2
+
+#connect to the db
+con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123')
+cur = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123').cursor()
