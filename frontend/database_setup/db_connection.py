d06a6905744f1e3e4b3a2db9274f9b96aa6016f1
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-02 11:04:42 +0200

    Fixed error with cur

--- frontend/database_setup/db_connection.py
+++ frontend/database_setup/db_connection.py
@@ -2,5 +2,5 @@
 import psycopg2
 
 #connect to the db
-con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123')
-cur = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123').cursor()
+con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=12345')
+cur = con.cursor()
