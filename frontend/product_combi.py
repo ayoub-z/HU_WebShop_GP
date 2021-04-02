5d3452b46e27d8536a82933b64606ceb0428996f
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-02 15:39:31 +0200

    Added function that keeps old recommendation

--- frontend/product_combi.py
+++ frontend/product_combi.py
@@ -2,15 +2,18 @@ import psycopg2
 from database_setup.db_connection import cur
 import sys
 
-def product_combi(lastcartproductid):
+def product_combi(lastcartproductid, place):
 	'''
 	Function returns the best 4 products that combine with the given product_id from shopping cart 
 	'''
-
+	lastcartproductid = lastcartproductid.replace("'","")
+	lastcartproductid = lastcartproductid.replace(" ","")
+	lastcartproductid = lastcartproductid.strip("[]").split(",")
+	lastcartproductid = lastcartproductid[place]	
 	# sql query retrieves all 4 products that are a good combination with this product_id from table "product_combination"
 	cur.execute("SELECT * FROM product_combination WHERE product_ID = %s", (str(lastcartproductid),))
 	productlist = cur.fetchall()
-	print(productlist, file=sys.stderr)
+	# print(productlist, file=sys.stderr)
 	# checks that the count of how often the last product is bought is at least 5 or higher
 	try:
 		if productlist[0][5] >= 5:
@@ -18,4 +21,20 @@ def product_combi(lastcartproductid):
 			return prodids
 	except Exception as e:
 		print(e, file=sys.stderr)
+		return None
+
+def product_combi_engine(lastcartproductid, lengthcart, place):
+	'''
+	This function makes use of the function "product_combi" so that if there isn't a recommendation found for the last product,
+	it tries to find a recommendation for the next product after it in the shoppingcart
+	'''
+	if product_combi(lastcartproductid, -1) != None:
+		return product_combi(lastcartproductid, -1)
+	elif lengthcart >= 2:
+		for i in range(2, lengthcart+1):
+			if product_combi(lastcartproductid, -i) != None:
+				return product_combi(lastcartproductid, -i)
+			else:
+				continue
+	else:
 		return None
