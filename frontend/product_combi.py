c3df062d8d7d403a7d030d077d52f2fac8d8581f
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-03 09:49:24 +0200

    Fixed sql queriy/tweaked requirement for algorithm

--- frontend/product_combi.py
+++ frontend/product_combi.py
@@ -14,11 +14,11 @@ def product_combi(lastcartproductid, place):
 
 	
 	# sql query retrieves all 4 products that are a good combination with this product_id from table "product_combination"
-	cur.execute("SELECT * FROM product_combination WHERE product_ID in (%s)", (similar_products))
+	cur.execute("SELECT * FROM product_combination WHERE product_ID = %s", (str(lastcartproductid),))
 	productlist = cur.fetchall()
 	# checks that the count of how often the last product is bought is at least 5 or higher
 	try:
-		if productlist[0][5] >= 5:
+		if productlist[0][5] >= 15:
 			prodids = productlist[0][1:5]
 			return prodids
 	except Exception as e:
