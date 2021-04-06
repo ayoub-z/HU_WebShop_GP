43b47123f0b041da93f947fac774eb314afe4522
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-06 20:14:01 +0200

    coppied/edited similarity_score function

---
+++ frontend/database_setup/similar_p_finder.py
@@ -0,0 +1,69 @@
+import psycopg2
+import itertools
+from db_connection import *
+
+def productfetcher(product_id):
+	'''this function fetches all products except the productid in the function arguments'''
+	cur = con.cursor()
+	cur.execute("SELECT _id, category, selling_price, doelgroep, sub_category, sub_sub_category FROM product WHERE _id != %s", (product_id,))
+	return cur.fetchall()
+
+def startproductfetcher(product_id):
+	'''this function fetches one specific product'''
+	cur = con.cursor()
+	cur.execute("SELECT _id, category, selling_price, doelgroep, sub_category, sub_sub_category FROM product WHERE _id = %s", (product_id,))
+	return cur.fetchone()
+
+def similarity_score(product_id):
+	'''this function assigns similarity score based on category, selling price, doelgroep, sub_cat and sub_sub_cat
+	it saves these scores in a dict and returns the top 4 products in that dict. 
+	Per product, we look at ALL other products and score them'''
+	similarity_score_dict = {}
+	productlist = productfetcher(product_id)
+	startproduct = startproductfetcher(product_id)
+
+	#this allows us to easily adjust the weight of certain attributes. Higher number = higher weight
+	categoryweight = 1
+	priceweight = 0.2
+	doelgroepweight = 5	
+	sub_categoryweight = 2
+	sub_sub_categoryweight = 5
+
+	for product in productlist:
+		if startproduct[3] == "vrouw" or startproduct[3] == "man":
+			if product[3] == startproduct[3]:
+				#sub_sub_category
+				similarity_score_dict[product[0]] = 0.1
+				#category points
+				if product[1] == startproduct[1]:
+					similarity_score_dict[product[0]] += categoryweight
+				#price within 20% of start?
+				if 110 > (100 * product[2]/startproduct[2]) > 90:
+					similarity_score_dict[product[0]] += priceweight
+				if product[4] == startproduct[4]:
+					similarity_score_dict[product[0]] += sub_categoryweight
+				#sub_sub_category
+				if product[5] == startproduct[5]:
+					similarity_score_dict[product[0]] += sub_sub_categoryweight
+					continue
+			else:
+				continue
+		#sub_sub_category
+		similarity_score_dict[product[0]] = 0.1
+		#category points
+		if product[1] == startproduct[1]:
+			similarity_score_dict[product[0]] += categoryweight
+		#price within 20% of start?
+		if 120 > (100 * product[2]/startproduct[2]) > 80:
+			similarity_score_dict[product[0]] += priceweight
+		#doelgroep
+		if product[3] == startproduct[3]:
+			similarity_score_dict[product[0]] += doelgroepweight
+		if product[4] == startproduct[4]:
+			similarity_score_dict[product[0]] += sub_categoryweight
+		#sub_sub_category
+		if product[5] == startproduct[5]:
+			similarity_score_dict[product[0]] += sub_sub_categoryweight
+	sorted_score_dict = sorted(similarity_score_dict.items(), key=lambda x: x[1], reverse=True)[:4]
+
+	return sorted_score_dict
